ad458526e4cc7a9df434ed4a2e5953cdcc859bfe
Yjiie <andre.kiseleov@yandex.ru>
2024-11-29 16:54:41 +0400

    Update 2 Laba.py

--- 2 Laba.py
+++ 2 Laba.py
@@ -9,35 +9,35 @@
 '''
 import re
 
-def digit_to_text(digit):
-    digit_dict = {
-        '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
-        '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
-    }
-    return digit_dict.get(digit, '')
+digit_dict = {
+    '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
+    '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
+}
 
 valid_numbers = []
 
 with open("text.txt", "r") as file:
     content = file.read()
-    pattern = r'\b[01]+\b'
+    pattern = r'\b-?[01]+\b'
     binary_numbers = re.findall(pattern, content)
 
     for number in binary_numbers:
-        if all(c in "01" for c in number):
+        if number[0] == '-':
+            decimal_value = -int(number[1:], 2)
+        else:
             decimal_value = int(number, 2)
-            if (
-                    decimal_value <= 2048 and
-                    decimal_value % 2 == 0 and
-                    len(number) > 1 and number[-2] == '0'
-            ):
-                valid_numbers.append(decimal_value)
-                print("Цифры числа, исключая нули:", " ".join(c for c in number if c != '0'))
+
+        if abs(decimal_value) <= 2048 and decimal_value % 2 == 0 and len(number) > 1 and number[-2] == '0':
+            valid_numbers.append(decimal_value)
+            filtered_digits = [c for c in number if c not in {'0', '-'}]
+            if filtered_digits:
+                print("Цифры числа, исключая нули:", " ".join(filtered_digits))
 
 if valid_numbers:
     min_value = min(valid_numbers)
     max_value = max(valid_numbers)
     avg_value = (min_value + max_value) // 2
-    print("Среднее число прописью:", " ".join(digit_to_text(d) for d in str(avg_value)))
+    avg_text = [digit_dict[digit] for digit in str(abs(avg_value))]
+    print("Среднее число прописью:", " ".join(avg_text))
 else:
     print("Не найдено подходящих чисел.")
